96ae9ececf8b3270d208dabb2eea7ccfa2df87c6
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2020-09-22 20:56:56 +0000

    Add augmentation

--- datasets/skyline12.py
+++ datasets/skyline12.py
@@ -2,6 +2,39 @@ from pathlib import Path
 from imageio import imread
 import numpy as np
 import scipy.io as sio
+import imgaug as ia
+import imgaug.augmenters as iaa
+
+
+_augmentor = iaa.Sequential([
+    iaa.MultiplyBrightness((0.7, 1.5)),
+    iaa.MultiplySaturation((0.0, 2.0)),
+    iaa.GammaContrast((0.2, 1.4)),
+    iaa.Sometimes(1.0, iaa.Crop(percent=(0.1, 0.3))),
+    iaa.HorizontalFlip(0.5)
+], random_order=True)
+
+
+def create_augment_fn(augmentor=_augmentor):
+    ia.random.seed(42)
+    augmentor = iaa.Sequential([
+        augmentor,
+        iaa.Resize(512)
+    ])
+
+    def augment(img, annot, seed, augmentor=augmentor):
+        segmap = np.dstack([annot, seed]).astype('uint8')
+        aug_img, [aug_segmap] = augmentor.augment(image=img, segmentation_maps=[segmap])
+        aug_annot, aug_seed = np.dsplit(aug_segmap, 2)
+        aug_annot = aug_annot.squeeze()
+        aug_seed = aug_seed.squeeze()
+        # some labels & scribbles are of bigger size so we need to forcefully
+        # resize them to 512x512
+        aug_annot = iaa.Resize(512, interpolation='nearest').augment_image(aug_annot)
+        aug_seed = iaa.Resize(512, interpolation='nearest').augment_image(aug_seed)
+        return aug_img, aug_annot, aug_seed
+
+    return augment
 
 
 class Skyline12:
@@ -25,9 +58,9 @@ class Skyline12:
         for path in images:
             image = imread(path)
             city = path.parts[-2]
-            annotation = self.root/'annotations'/city/f'label_{path.stem}.mat'
+            annotation = self.root / 'annotations' / city / f'label_{path.stem}.mat'
             annotation = sio.loadmat(annotation)['labels']
-            seed = self.root/'seeds'/city/f'{path.stem}_fgpixels.mat'
+            seed = self.root / 'seeds' / city / f'{path.stem}_fgpixels.mat'
             seed = sio.loadmat(seed)['fgpixels']
             seed = self._seed_as_img(seed, annotation.shape)
             yield image, annotation, seed
