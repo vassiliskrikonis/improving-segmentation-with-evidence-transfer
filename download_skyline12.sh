20d19f4e54cf737a81f5c1a3e1fac2bcc61158cf
Vassis Krikonis <vassiliskrikonis@gmail.com>
2020-10-04 10:50:57 +0000

    fixup! Add loader for skyline12

--- download_skyline12.sh
+++ download_skyline12.sh
@@ -1,3 +1,4 @@
 curl https://ttic.uchicago.edu/~smaji/projects/skylineParsing/skyline12.tar.gz --output skyline12.tar.gz
+mkdir -p datasets/skyline12
 tar xf skyline12.tar.gz -C datasets/skyline12/
 rm skyline12.tar.gz
