59b1ad0f46f62f5732e4e336c379fcae4f47409c
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2020-10-18 22:17:59 +0000

    Log training in wandb
    Add keras callback to log plots to WandB

--- callbacks.py
+++ callbacks.py
@@ -1,6 +1,7 @@
 from utils import plot_to_image
 from datasets.skyline12 import Skyline12
 import tensorflow as tf
+import wandb
 
 
 class LogImages(tf.keras.callbacks.Callback):
@@ -23,3 +24,22 @@ class LogImages(tf.keras.callbacks.Callback):
                 figs.append(fig)
             tf.summary.image(
                 'Predictions', figs, max_outputs=len(preds), step=epoch)
+
+
+class LogImagesWandb(tf.keras.callbacks.Callback):
+    def __init__(self, pred_data):
+        self.pred_data = pred_data
+
+    def on_epoch_end(self, epoch, logs=None):
+        xs, ys = self.pred_data
+        preds = self.model(xs, training=False)
+        figs = []
+        for x, y, pred_y in zip(xs, ys, preds):
+            fig = Skyline12.show_sample(x, [y, pred_y], from_tensors=True)
+            fig = plot_to_image(fig)
+            # remove the batch dimensions added in plot_to_image, since
+            # we add multiple images after all
+            fig = tf.squeeze(fig, axis=0)
+            figs.append(fig)
+        wandb.log(
+            {'predictions': [wandb.Image(fig) for fig in figs]}, step=epoch)
