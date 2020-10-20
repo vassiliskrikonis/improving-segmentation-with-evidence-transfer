adfa30290e6681007d127e1905e7f644274c1df7
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2020-10-20 11:05:24 +0000

    Dijoint update on two branches (unet & q)

--- models/evitram.py
+++ models/evitram.py
@@ -12,13 +12,22 @@ class EvidenceTransferModel(tf.keras.Model):
 
     def train_step(self, data):
         x, [y, v] = data
-        with tf.GradientTape() as tape:
+        with tf.GradientTape() as unet_tape, tf.GradientTape() as q_tape:
             [y_pred, v_pred] = self(x, training=True)
             unet_loss = tf.reduce_mean(self.unet_loss(y, y_pred))
             q_loss = tf.reduce_mean(self.q_loss(v, v_pred))
             loss = unet_loss + self.loss_lambda * q_loss
-        grads = tape.gradient(loss, self.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+        # apply gradients of loss wrt unet weights
+        unet_vars = self.get_layer('Unet').trainable_variables
+        unet_grads = unet_tape.gradient(loss, unet_vars)
+        self.optimizer.apply_gradients(zip(unet_grads, unet_vars))
+
+        # apply gradients of loss wrt q weights
+        q_vars = self.get_layer('Q').trainable_variables
+        q_grads = q_tape.gradient(loss, q_vars)
+        self.optimizer.apply_gradients(zip(q_grads, q_vars))
+
         self.compiled_metrics.update_state([y, v], [y_pred, v_pred])
         return {'loss': loss, **{m.name: m.result() for m in self.metrics}}
 
@@ -46,10 +55,13 @@ def create_evidence_transfer_model(
         outputs=connecting_layer.output,
         name='Unet_intermid'
     )
+    q = tf.keras.Sequential([
+        unet_intermid,
+        q_model
+    ], name='Q')
 
     x = unet(img)
-    v = unet_intermid(img)
-    v = q_model(v)
+    v = q(img)
 
     return EvidenceTransferModel(
         img,
